ae94894a8321d198da1330fb2a06e4e39757b84d
designmarlon <70332243+designmarlon@users.noreply.github.com>
2020-09-24 08:54:04 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  offset: 57,
+});
+AOS.init();
+$(document).ready(function () {
+  $(".carousel-servicios").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 800,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+
+  $(".carousel-majo").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 800,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
